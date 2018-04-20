53549ccfd4d49f93d1d82da7d55c475977a6c96e
John Ratcliff <john.w.ratcliff@biola.edu>
2018-04-20 17:42:05 +0000

    Update google constructor

--- addon-test-support/stubs/google-chart.js
+++ addon-test-support/stubs/google-chart.js
@@ -2,44 +2,42 @@ export default function stubGoogleCharts() {
   const stubDraw = {
     draw() {}
   };
+  const factory = window.google = function() {};
 
-  window.google = {
-    load(chart, num, options) {
-      if (options.callback) {
-        options.callback();
-      }
-    },
-
-    visualization: {
-      'ColumnChart'() {
-        return stubDraw;
-      },
-      'LineChart'() {
-        return stubDraw;
-      },
-      'BarChart'() {
-        return stubDraw;
-      },
-      'PieChart'() {
-        return stubDraw;
-      },
-      'AreaChart'() {
-        return stubDraw;
-      },
-
-      NumberFormat() {
-        return {
-          format() {}
-        };
-      },
-
-      DataTable() {
-        return {
-          addColumn() {},
-          addRows() {},
-          addRow() {},
-        };
-      },
+  factory.load = function(chart, num, options) {
+    if (options.callback) {
+      options.callback();
     }
+  };
+
+  factory.visualization = function() {};
+  factory.visualization.ColumnChart = function() {
+    return stubDraw;
+  };
+  factory.visualization.LineChart = function() {
+    return stubDraw;
+  };
+  factory.visualization.BarChart = function() {
+    return stubDraw;
+  };
+  factory.visualization.PieChart = function() {
+    return stubDraw;
+  };
+  factory.visualization.AreaChart = function() {
+    return stubDraw;
+  };
+
+  factory.visualization.NumberFormat = function() {
+    return {
+      format() {}
+    };
+  };
+
+  factory.visualization.DataTable = function() {
+    return {
+      addColumn() {},
+      addRows() {},
+      addRow() {},
+    };
   };
 }
